89db4695cbe32fe1c3725ccb242cc38d1f2a5e28
AdityaRevadkar <129678008+AdityaRevadkar@users.noreply.github.com>
2023-09-05 18:36:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,20 @@ const App = () => {
 //code here 
 
   const settingValue = () => {
-    const inputValue = inputRef.current.querySelector("input").value;
+    const inputValue = inputRef.current.value;
     const textarea = document.getElementById("textarea");
     textarea.value = inputValue;
   };
 
   const focusInput = () => {
-    inputRef.current.querySelector("input").focus();
+    inputRef.current.focus();
   };
  
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/><br/>
+    <InputField  ref={newRef} type="text"/> <br/>
     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
+    <button id="focusInputButton" onClick={focusInput}>Focus the input</button> <br/> <br/>
      <textarea id="textarea"></textarea>
 
     </div>
